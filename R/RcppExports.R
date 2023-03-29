70be86a120743061c48edb69878474f59399de3f
Ciar Noble <92942535+ciardorje@users.noreply.github.com>
2023-03-29 12:06:36 +0100

    Added maximum focal

--- R/RcppExports.R
+++ R/RcppExports.R
@@ -21,6 +21,10 @@ meanfocal_cpp <- function(rasterband, weights, wrow, wcol, band) {
     .Call('_velox_meanfocal_cpp', PACKAGE = 'velox', rasterband, weights, wrow, wcol, band)
 }
 
+maxfocal_cpp <- function(rasterband, weights, wrow, wcol, band) {
+    .Call('_velox_maxfocal_cpp', PACKAGE = 'velox', rasterband, weights, wrow, wcol, band)
+}
+
 hittest_cpp <- function(rasterbands, dim, extent, res, polyX, polyY, polyCorners) {
     .Call('_velox_hittest_cpp', PACKAGE = 'velox', rasterbands, dim, extent, res, polyX, polyY, polyCorners)
 }
