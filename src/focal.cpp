fd6ab13deb798b1b750f28291218265684fa5790
Ciar Noble <92942535+ciardorje@users.noreply.github.com>
2023-03-29 12:04:05 +0100

    Added maximum focal

--- src/focal.cpp
+++ src/focal.cpp
@@ -108,4 +108,40 @@ NumericMatrix meanfocal_cpp(NumericMatrix rasterband, NumericMatrix weights, int
   return(newband);
 }
 
+// [[Rcpp::export]]
+NumericMatrix maxfocal_cpp(NumericMatrix rasterband, NumericMatrix weights, int wrow, int wcol, int band) {
+
+  int nrow = rasterband.nrow();
+  int ncol = rasterband.ncol();
+
+  NumericMatrix newband(nrow, ncol);
+  vector<double> v;
+
+  int idim = (wrow-1)/2;
+  int jdim = (wcol-1)/2;
+
+  for (int i = 0; i < nrow; i++) {
+    int imin = i - idim;
+    int imax = i + idim + 1;
+    for (int j = 0; j < ncol; j++) {
+      int jmin = j - jdim;
+      int jmax = j + jdim + 1;
+      int wicounter = 0;	
+      for (int wi = imin; wi < imax; wi++) {
+	int wjcounter = 0;
+        for (int wj = jmin; wj < jmax; wj++) {
+          if (wi >= 0 && wi < nrow && wj >= 0 && wj < ncol) {
+            v.push_back(rasterband(wi, wj)*weights(wicounter, wjcounter));
+          }
+	  wjcounter++;
+        }
+	wicounter++;
+      }
+      newband(i,j) = *max_element(v.begin(), v.end());
+      v.clear();
+    }
+  }
+  return(newband);
+}
+
 
